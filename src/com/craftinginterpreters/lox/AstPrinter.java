e9822e83f72f2e51da896ba398d995a0a9af171b
zgoksu <zafergoksu00@gmail.com>
2022-04-18 00:50:21 +0100

    Fix formatting on AstPrinter

--- src/com/craftinginterpreters/lox/AstPrinter.java
+++ src/com/craftinginterpreters/lox/AstPrinter.java
@@ -17,7 +17,9 @@ public class AstPrinter implements Expr.Visitor<String> {
 
     @Override
     public String visitLiteralExpr(Expr.Literal expr) {
-        if (expr.value == null) return "nil";
+        if (expr.value == null) {
+            return "nil";
+        }
         return expr.value.toString();
     }
 
@@ -44,13 +46,10 @@ public class AstPrinter implements Expr.Visitor<String> {
         Expr expression = new Expr.Binary(
                 new Expr.Unary(
                         new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)
-                ),
+                        new Expr.Literal(123)),
                 new Token(TokenType.STAR, "*", null, 1),
                 new Expr.Grouping(
-                        new Expr.Literal(45.67)
-                )
-        );
+                        new Expr.Literal(45.67)));
 
         System.out.println(new AstPrinter().print(expression));
     }
