063b82e63067bd0298e025b2378de9cea1e29166
zgoksu <zafergoksu00@gmail.com>
2022-06-18 00:10:17 +0100

    Add class and class instance scaffolding

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -20,6 +20,9 @@ public class Parser {
 
     private Stmt declaration() {
         try {
+            if (match(TokenType.CLASS)) {
+                return classDeclaration();
+            }
             if (match(TokenType.FUN)) {
                 return function("function");
             }
@@ -31,6 +34,20 @@ public class Parser {
             synchronize();
             return null;
         }
+    }
+
+    private Stmt classDeclaration() {
+        Token name = consume(TokenType.IDENTIFIER, "Expect class name.");
+        consume(TokenType.LEFT_BRACE, "Expect '{' before class body.");
+
+        List<Stmt.Function> methods = new ArrayList<>();
+        while (!check(TokenType.RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(TokenType.RIGHT_BRACE, "Expect '}' after class body.");
+
+        return new Stmt.Class(name, methods);
     }
 
     private Stmt statement() {
